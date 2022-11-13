937259862b342f8ce25a6b360e56986db3f281a6
Ivan <ifucin@gmail.com>
2022-11-13 19:46:44 +0300

    add app core add database

---
+++ core/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
