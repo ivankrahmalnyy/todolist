5c1488e97dd7100472b0472aab0f1dffc8664fb7
Ivan <ifucin@gmail.com>
2023-01-05 20:48:51 +0300

    fix

--- bot/tg/client.py
+++ bot/tg/client.py
@@ -1,10 +1,5 @@
 import requests
 
-#from bot.tg.dc import GetUpdatesResponse
-
-
-#from todolist.bot.tg.dc import GetUpdatesResponse
-
 
 from bot.tg.dc import GetUpdatesResponse, SendMessageResponse, get_updates_schema, send_message_schema
 
@@ -15,22 +10,14 @@ class TgClient:
 
     def get_url(self, method: str) -> str:
         """
-            URL метод для запроса к telegram боту
-            Args:
-                method: какой запрос будет отправлен боту
-            Returns:
-                str
+        URL метод для запроса к telegram боту.
         """
         return f"https://api.telegram.org/bot{self.token}/{method}"
 
     def get_updates(self, offset: int = 0, timeout: int = 60) -> GetUpdatesResponse:
         """
             Получения входящих обновлений от пользователя.
-            Args:
-                offset: int
-                timeout: int
-            Returns:
-                GetUpdatesResponse
+
         """
         response = requests.get(self.get_url(f"getUpdates?offset={offset}&timeout={timeout}"))
         json_data = response.json()
@@ -41,11 +28,7 @@ class TgClient:
     def send_message(self, chat_id: int, text: str) -> SendMessageResponse:
         """
             Отправление сообщения пользователю от бота.
-            Args:
-                chat_id: int
-                text: int
-            Returns:
-                SendMessageResponse
+
         """
         response = requests.get(self.get_url(f"sendMessage?chat_id={chat_id}&text={text}"))
         json_data = response.json()
